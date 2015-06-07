81dbc04ccb56b1927912180b4c38adc1e4cba6d5
Shawn Lutch <shawn.lutch94@gmail.com>
2015-06-07 17:41:07 -0500

    refactoring
    add plugged-in icon logic and refactor code

--- src/battery.h
+++ src/battery.h
@@ -1,5 +1,10 @@
 #include <pebble.h>
 
+/*
+ *	battery.h
+ *		Contains update procedures for battery and plugged-icon layers
+ */
+
 #define BATTERY_METER_MAX_WIDTH  124
 #define BATTERY_TOP_LINE_X       97
 #define BATTERY_METER_LINE_X     98
@@ -10,23 +15,17 @@ static int CALC_BATTERY_METER_WIDTH(int battery)
 	return (int)(BATTERY_METER_MAX_WIDTH * pct);
 }
 
-static void update_battery(Layer *this_layer)
-{
-	layer_mark_dirty(this_layer);
-}
-
 static void update_battery_proc(Layer *this_layer, GContext *ctx)
 {
 	graphics_context_set_stroke_color(ctx, GColorWhite);
-	
+
 	GPoint top_left  = GPoint(10, BATTERY_TOP_LINE_X);
 	GPoint top_right = GPoint(10 + BATTERY_METER_MAX_WIDTH,
 							  BATTERY_TOP_LINE_X);
 	graphics_draw_line(ctx, top_left, top_right);
-	
-	int battery = battery_state_service_peek().charge_percent;
+
 	GPoint meter_left  = GPoint(10, BATTERY_METER_LINE_X);
-	GPoint meter_right = GPoint(10 + CALC_BATTERY_METER_WIDTH(battery),
+	GPoint meter_right = GPoint(10 + CALC_BATTERY_METER_WIDTH(s_battery_pct),
 								BATTERY_METER_LINE_X);
 	graphics_draw_line(ctx, meter_left, meter_right);
 }
--- src/ui.h
+++ src/ui.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/*
+ *	ui.h
+ * 		Contains functions used to update the user interface
+ */
+
 #include "fields.h"
 
 void update_inversion()
@@ -11,10 +16,24 @@ void update_inversion()
 	layer_set_hidden((Layer *)s_inverter_layer, !invert);
 }
 
+static void update_battery()
+{
+	layer_mark_dirty(s_battery_layer);
+}
+
+void update_plugged()
+{
+	s_plugged = battery_state_service_peek().is_plugged;
+
+	// hide if not plugged in
+	layer_set_hidden((Layer *)s_plugged_layer, !s_plugged);
+}
+
 void update_ui()
 {
   // update ui elements
 	update_inversion();
+	update_plugged();
 
   // flash the backlight to alert that we've updated
 	light_enable_interaction();
